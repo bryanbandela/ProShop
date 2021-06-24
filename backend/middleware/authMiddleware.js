cdcffebcc32e75bf63b852420ddf6c889880db7b
Bryan Bandela <bryan.bandela@gmail.com>
2021-06-24 18:21:54 +0200

    Add Admin screen Part 1

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -33,4 +33,14 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  //to make sure the user is registered/exist and is an admin
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+export { protect, admin };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,16 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers); //the same api has a post & get method but the get method goes thru a middleware
 //it will use /api/users then /login
 router.post('/login', authUser);
 //protect is a middle from the folder middleware
@@ -17,5 +21,11 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
